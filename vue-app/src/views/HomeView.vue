d93c75cdf078a44f4dc458ece99d1ec9461eacc2
VyTT <40004136+tranVyTDT@users.noreply.github.com>
2023-06-05 22:45:32 +0700

    [VyTT][Post]: create Post component

--- vue-app/src/views/HomeView.vue
+++ vue-app/src/views/HomeView.vue
@@ -1,3 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import ListPost from "../components/ListPost.vue";
+    import PostFilter from "../components/PostFilter.vue";
+</script>
 
-<template></template>
+<template>
+    <section class="pb-0 py-sm-5">
+        <div class="container">
+            <div class="row">
+                <ListPost/>
+                <PostFilter/>
+            </div>
+        </div>
+    </section>
+</template>
